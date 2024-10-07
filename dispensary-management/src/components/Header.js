fd2342e0c0c0c9bb32782a5fb488e8e7b584f1cd
786Aaqueeb <97385514+786Aaqueeb@users.noreply.github.com>
2024-10-07 13:18:18 +0530

    Added 'Dashboard' button.

--- dispensary-management/src/components/Header.js
+++ dispensary-management/src/components/Header.js
@@ -18,17 +18,23 @@ const Header = ({ isLoggedIn, handleLogout }) => {
         <Link to="/" className="text-xl md:text-2xl font-bold flex items-center">
           <img src="/logo3.webp" alt="Logo" className="h-12 w-12 bg-blue-600 inline mr-2" />
           <span className="hidden md:inline">Dispensary Management System</span>
-
           <span className="md:hidden">DMS</span> {/* Shorter text for mobile */}
         </Link>
-        <div>
+        <div className='flex space-x-4'>
           {isLoggedIn ? (
+            <>
+            <Link
+              to="/appointment"
+              className="bg-green-500 px-6 py-3 rounded hover:bg-green-600 transition duration-100"
+            >
+              Dashboard
+            </Link>
             <button
               onClick={onLogoutClick}
-              className="bg-orange-500 px-6 py-3 rounded hover:bg-red-600 transition duration-100"
-            >
+              className="bg-orange-500 px-6 py-3 rounded hover:bg-red-600 transition duration-100">
               Logout
             </button>
+            </>
           ) : (
             <Link to="/" className="bg-green-500 px-6 py-3 rounded hover:bg-green-600 transition duration-100">
               Login
